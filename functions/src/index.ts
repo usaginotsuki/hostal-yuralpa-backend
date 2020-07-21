8ca6380b519c71a1886433797931dc4a80c5f08c
usaginotsuki <jcchachalo@gmail.com>
2020-07-20 20:59:10 -0500

    bug swal fixed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as admin from 'firebase-admin';
+import * as cors from 'cors';
 
 admin.initializeApp(functions.config().firebase);
 
@@ -9,6 +10,7 @@ const db=admin.firestore();
 db.settings({ignoreUndefinedProperties:true});
 
 const main=express();
+main.use(cors());
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({extended:false}));
 main.use('/api',require('./usuario').routes);
@@ -16,6 +18,21 @@ main.use('/api',require('./usuario').routes);
 main.use('/api',require('./habitacion').routes);
 
 */
+export interface Mensaje{
+    title:string,
+    text:string,
+    icon:string
+}
+
+export function Message(title:string, text:string, icon: string){
+    let message :Mensaje = {
+        title: title,
+        text: text,
+        icon: icon
+    }
+    return message;
+}
+
 export const api=functions.https.onRequest(main);
 export{db};
 // // Start writing Firebase Functions
